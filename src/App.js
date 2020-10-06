9c95df0a19fed507240b076157e6764f0261b824
cmanthou <cory.manthou@gmail.com>
2020-10-05 20:50:24 -0400

    Copied Files from Desktop

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import FilterInput from "./FilterInput";
+import Table from "./Table";
+import { getUsers } from "./API";
+import "./App.css";
+
+function App() {
+    const [initialUsers, updateAvailableUsers] = useState ([]);
+    const [usersToRender, updateUsersToRender] = useState ([]);
+
+    useEffect(() => {
+        getUsers().then(({ data: { results } }) => updateAvailableUsers(results));
+    }, []);
+
+    return (
+        <div className="App">
+            <h1>Employee Directory</h1>
+            <p>Please input User's first name to filter by first name.</p>
+            <FilterInput users={initialUsers} updateUsers={updateUsersToRender} />
+            <Table users={usersToRender} />
+        </div>
+    );
+}
+export default App;
